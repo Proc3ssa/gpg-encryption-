52da213448e7b2ce7e5e814898389e870aa9878a
Processor <59508966+Proc3ssa@users.noreply.github.com>
2022-06-09 19:05:19 +0100

    Add files via upload

---
+++ encrypter.sh
@@ -0,0 +1,21 @@
+echo "Select what you want to do"
+select option in Encrypt Decrypt
+  do
+    echo "You have selected $option"
+    if [ $REPLY = 1 ]
+    then
+        read -p "Enter file name: " filename
+        gpg -c $filename
+        echo "Encryption done"
+        rm $filename
+
+    else
+
+        read -p "Enter file name: " filename
+        gpg -d $filename
+        echo "Decryption done"
+        rm $filename
+
+
+fi
+done
